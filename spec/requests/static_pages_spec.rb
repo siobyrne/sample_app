51aa9d788c198a45522d525716154203a151311e
siobyrne <byrne.sio@gmail.com>
2014-07-21 22:31:49 -0400

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,4 +35,15 @@ describe "Static pages" do
 	       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 	     end
   end
+  describe "Contact Us page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+	  it "should have the title 'Contact Us'" do
+	       visit '/static_pages/contact'
+	       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+	     end
+  end
 end
